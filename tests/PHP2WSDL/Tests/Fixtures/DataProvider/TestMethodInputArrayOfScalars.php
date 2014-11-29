492f2d6776d8b6979b9d15afa7bac0ce6e0f6414
Dragos Protung <dragos@protung.ro>
2014-11-29 15:05:59 +0100

    Added test for simple array as PHP class method input.

---
+++ tests/PHP2WSDL/Tests/Fixtures/DataProvider/TestMethodInputArrayOfScalars.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace PHP2WSDL\Tests\Fixtures\DataProvider;
+
+class TestMethodInputArrayOfScalars
+{
+
+    /**
+     * @param array $array Simple array
+     */
+    public function inputArraySimple(array $array) {
+
+    }
+}
