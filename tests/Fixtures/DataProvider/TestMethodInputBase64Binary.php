5590d80e2f773ff13c8f33af3940deae12e6e50d
jsoumelidis <jsoumelidis@gmail.com>
2016-03-11 00:13:28 +0200

    WSDL: base64binary type added (xsd:base64Binary)

---
+++ tests/Fixtures/DataProvider/TestMethodInputBase64Binary.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace PHP2WSDL\Test\Fixtures\DataProvider;
+
+class TestMethodInputBase64Binary {
+
+    /**
+     * @param base64Binary $base64binary Input.
+     */
+    public function inputBase64Binary($base64binary)
+	{
+    }
+	
+	/**
+     * @param base64Binary[] $base64binaries Input.
+     */
+    public function inputBase64BinaryArray(array $base64binaries)
+	{
+    }
+}
